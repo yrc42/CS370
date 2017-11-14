81981b01030b91edcac41897a33ff718db3c1db1
yrc42 <yr5chen@edu.uwaterloo.ca>
2017-11-14 00:48:12 -0500

    unlock

--- a4/unlock.m
+++ a4/unlock.m
@@ -7,8 +7,9 @@ hold on; %
 
 
 % --- YOUR CODE HERE ---
-t = [0 795 1447 2172 2897]
-x = [0 27 50 -4 22]
-y = [-1 37 27 11 42]
-yy = spline(x,y);
-plot(t, x, y );
+t = [0 795 1447 2172 2897];
+x = [0 27 50 -4 22];
+y = [-1 37 27 11 42];
+x_cs = spline(t, [0 x 0]);
+y_cs = spline(x,[0 y 0]);
+plot(x_cs, y_cs );
