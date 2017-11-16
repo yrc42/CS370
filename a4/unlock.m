346593f66ce1a3bdb368d2b95c44dacf50039b95
yrc42 <yr5chen@edu.uwaterloo.ca>
2017-11-16 14:29:57 -0500

    Update unlock.m

--- a4/unlock.m
+++ a4/unlock.m
@@ -11,13 +11,19 @@ t = [0 795 1447 2172 2897];
 x = [0 27 50 -4 22];
 y = [-1 37 27 11 42];
 x_cs = spline(t, [0 x 0]); 
-y_cs = spline(t,[0 y 0]); 
-tref = 1:1:2897;
+y_cs = spline(t,[0 y 0]);
 N = length(t);
+ref = round(1000/(N-1));
+tref = zeros(1,1000);
+for k = 1: N-1
+    for i= 1:ref
+        tref((k-1)*ref+i) = t(k)+ (i-1)*(t(k+1)-t(k))/ref;
+    end
+end
+tref(tref==0) = [];
 xr = zeros(length(tref));
 yr = zeros(length(tref));
- 
-for j =1:2897
+for j =1:length(tref)
   xr(j) = ppval(x_cs, tref(j));
   yr(j) = ppval(y_cs, tref(j));
 end
