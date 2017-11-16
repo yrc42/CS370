f143f05004e2fbb269199e9564e1bb9d9c2a5e4b
yrc42 <yr5chen@edu.uwaterloo.ca>
2017-11-16 14:06:57 -0500

    Update unlock.m

--- a4/unlock.m
+++ a4/unlock.m
@@ -12,12 +12,13 @@ x = [0 27 50 -4 22];
 y = [-1 37 27 11 42];
 x_cs = spline(t, [0 x 0]); 
 y_cs = spline(t,[0 y 0]); 
+tref = 1:1:2897;
 N = length(t);
-xr = zeros(N);
-yr = zeros(N);
+xr = zeros(length(tref));
+yr = zeros(length(tref));
  
-for j =1:N
-  xr(j) = ppval(x_cs, t(j));
-  yr(j) = ppval(y_cs, t(j));
+for j =1:2897
+  xr(j) = ppval(x_cs, tref(j));
+  yr(j) = ppval(y_cs, tref(j));
 end
 plot(xr, yr);
